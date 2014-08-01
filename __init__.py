0a92dcf577b969f648cd2eae96c8b1e591c4dba8
Piotr Adamowicz <padamowicz@gmail.com>
2014-08-01 19:25:10 +0200

    support for multipart baking

--- __init__.py
+++ __init__.py
@@ -14,8 +14,11 @@ import bpy
 from bpy.props import *
 from bpy.utils import register_class, unregister_class
 
-class BakeQueue(bpy.types.PropertyGroup):
-    a = 3
+class BakePair(bpy.types.PropertyGroup):
+    lowpoly = bpy.props.StringProperty(name="", description="", default="")
+    highpoly = bpy.props.StringProperty(name="", description="", default="")
+    hp_obj_vs_group = EnumProperty(name="Object or Group", description="", default="OBJ", items = [('OBJ', '', 'Object', 'MESH_CUBE', 0), ('GRP', '', 'Group', 'GROUP', 1)])
+register_class(BakePair)
     
 class BakeJobSettings(bpy.types.PropertyGroup):
     bl_idname = __name__
@@ -60,15 +63,22 @@ class BakeJobSettings(bpy.types.PropertyGroup):
                             default = '//file.png',
                             subtype = 'FILE_PATH')
     
-    lowpoly = bpy.props.StringProperty(name="", description="", default="")
-    highpoly = bpy.props.StringProperty(name="", description="", default="")
-    hp_obj_vs_group = EnumProperty(name="Object or Group", description="", default="OBJ", items = [('OBJ', '', 'Object', 'MESH_CUBE', 0), ('GRP', '', 'Group', 'GROUP', 1)])
+    #lowpoly = bpy.props.StringProperty(name="", description="", default="")
+    #highpoly = bpy.props.StringProperty(name="", description="", default="")
+    #hp_obj_vs_group = EnumProperty(name="Object or Group", description="", default="OBJ", items = [('OBJ', '', 'Object', 'MESH_CUBE', 0), ('GRP', '', 'Group', 'GROUP', 1)])
+    
+    bake_queue = bpy.props.CollectionProperty(type=BakePair)
+    #print(bake_queue)
+    #bake_queue.add()
+    #default_pair.lowpoly = ""
     #hp_obj_vs_group = bpy.props.IntProperty(name="Resolution Y",
     #                                            default = 1024)
     #row.prop_search(m, "sources_chosen_object", scene, "objects")
     
 register_class(BakeJobSettings)
 bpy.types.Scene.bakejob_settings = PointerProperty(type = BakeJobSettings)
+
+#bpy.types.Scene.bakejob_settings = new BakeJobSettings
 
 class BakeToolsBakeOp(bpy.types.Operator):
     '''Bakes'''
@@ -108,21 +118,25 @@ class BakeToolsBakeOp(bpy.types.Operator):
         
         bpy.data.scenes[0].cycles.bake_type = bjs.bake_pass
         
-        # make selections
-        bpy.ops.object.select_all(action='DESELECT')
-        # !!!! hardcoded scene name !!!!
-        if bjs.hp_obj_vs_group == "GRP":
-            for object in bpy.data.groups[bjs.highpoly].objects:
-                object.select = True
-        else:
-            bpy.data.scenes["Scene"].objects[bjs.highpoly].select = True
-        
-        bpy.data.scenes["Scene"].objects[bjs.lowpoly].select = True
-        bpy.context.scene.objects.active = bpy.data.scenes["Scene"].objects[bjs.lowpoly]
-        
-        
-        #bake
-        bpy.ops.object.bake(type=context.scene.cycles.bake_type, filepath="", width=bjs.resolution_x, height=bjs.resolution_y, margin=16, use_selected_to_active=True, cage_extrusion=1, cage_object="", normal_space='TANGENT', normal_r='POS_X', normal_g='POS_Y', normal_b='POS_Z', save_mode='INTERNAL', use_clear=False, use_cage=False, use_split_materials=False, use_automatic_name=False)
+        clear = True
+        for i, pair in enumerate(bjs.bake_queue):
+            # make selections
+            bpy.ops.object.select_all(action='DESELECT')
+            # !!!! hardcoded scene name !!!!
+            if pair.hp_obj_vs_group == "GRP":
+                for object in bpy.data.groups[pair.highpoly].objects:
+                    object.select = True
+            else:
+                bpy.data.scenes["Scene"].objects[pair.highpoly].select = True
+            
+            bpy.data.scenes["Scene"].objects[pair.lowpoly].select = True
+            bpy.context.scene.objects.active = bpy.data.scenes["Scene"].objects[pair.lowpoly]
+            
+            if i>0:
+                clear = False
+            
+            #bake
+            bpy.ops.object.bake(type=context.scene.cycles.bake_type, filepath="", width=bjs.resolution_x, height=bjs.resolution_y, margin=16, use_selected_to_active=True, cage_extrusion=1, cage_object="", normal_space='TANGENT', normal_r='POS_X', normal_g='POS_Y', normal_b='POS_Z', save_mode='INTERNAL', use_clear=clear, use_cage=False, use_split_materials=False, use_automatic_name=False)
         
         #save resulting image
         bpy.data.images['target'].save()
@@ -149,16 +163,24 @@ class BakeToolsPanel(bpy.types.Panel):
         row.alignment = 'EXPAND'
         row.operator("baketools.bake", text='Bake')
         
-        row = layout.row(align=True)
-        row.alignment = 'EXPAND'
-        row.prop_search(bjs, "lowpoly", bpy.context.scene, "objects")
+        for i, pair in enumerate(bjs.bake_queue):
+            box = layout.box().column(align=True)
+            row = box.row(align=True)
+            row.alignment = 'EXPAND'
+            row.prop_search(pair, "lowpoly", bpy.context.scene, "objects")
+                
+            row = box.row(align=True)
+            row.prop(pair, 'hp_obj_vs_group', expand=True)
+            if pair.hp_obj_vs_group == 'OBJ':
+                row.prop_search(pair, "highpoly", bpy.context.scene, "objects")
+            else:
+                row.prop_search(pair, "highpoly", bpy.data, "groups")
+            rem = row.operator("baketools.rem_pair", text = "", icon = "X")
+            rem.pair_index = i
         
         row = layout.row(align=True)
-        row.prop(bjs, 'hp_obj_vs_group', expand=True)
-        if bjs.hp_obj_vs_group == 'OBJ':
-            row.prop_search(bjs, "highpoly", bpy.context.scene, "objects")
-        else:
-            row.prop_search(bjs, "highpoly", bpy.data, "groups")
+        row.alignment = 'EXPAND'
+        row.operator("baketools.add_pair")
         
         row = layout.row(align=True)
         row.alignment = 'EXPAND'
@@ -173,11 +195,32 @@ class BakeToolsPanel(bpy.types.Panel):
         row.alignment = 'EXPAND'
         row.prop(bjs, 'bake_pass')
         
+class BakeToolsAddPairOp(bpy.types.Operator):
+    '''add pair'''
 
+    bl_idname = "baketools.add_pair"
+    bl_label = "Add Pair"
+    
+    def execute(self, context):
+        bpy.data.scenes[0].bakejob_settings.bake_queue.add()
         
+        return {'FINISHED'}
+
+class BakeToolsRemPairOp(bpy.types.Operator):
+    '''delete pair'''
+
+    bl_idname = "baketools.rem_pair"
+    bl_label = "Remove Pair"
+    
+    pair_index = bpy.props.IntProperty()
+    def execute(self, context):
+        bpy.data.scenes[0].bakejob_settings.bake_queue.remove(self.pair_index)
+        
+        return {'FINISHED'}
     
 def register():
     bpy.utils.register_module(__name__)
+    #bpy.data.scenes[0].bakejob_settings.bake_queue.add()
     
     
 def unregister():
