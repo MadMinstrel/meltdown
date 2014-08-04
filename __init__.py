3c7e2a982ac82e659b5bcafe5a070f665edcb1ee
Piotr Adamowicz <padamowicz@gmail.com>
2014-08-04 11:17:03 +0200

    Add license block

--- __init__.py
+++ __init__.py
@@ -1,13 +1,33 @@
+#  (c) 2014 by Piotr Adamowicz (MadMinstrel)
+
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 bl_info = {
     "name": "Bake Tools",
     "author": "Piotr Adamowicz",
-    "version": (0, 1),
+    "version": (0, 2),
     "blender": (2, 7, 1),
-    "location": "  ",
-    "description": "  ",
+    "location": "Properties Editor -> Render Panel",
+    "description": "Improved baking UI",
     "warning": "",
     "wiki_url": "",
-    "tracker_url": "",
+    "tracker_url": "https://github.com/MadMinstrel/bake-tools/issues",
     "category": "Baking"}
 
 import code
